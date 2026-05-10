f663be0470b76e10bb8efeae3313f6b705b1e0c9
Gergely Bertalan <bertalanofficial@gmail.com>
2026-05-10 12:06:40 +0200

    Add another box with hardcoded values as visual dev help

--- hornet/control_layer/gridcontrol.cpp
+++ hornet/control_layer/gridcontrol.cpp
@@ -22,6 +22,16 @@ void GridControl::init()
                          {"line one: this is the first line.",
                           "line two: and this is the second line.",
                           "line three: I can write y n m and more!"});
+
+    m_gridService.addBox(12,
+                         18,
+                         25,
+                         15,
+                         "my_second_box.txt",
+                         {"line one: this is the first line.",
+                          "line two: and this is the second line.",
+                          "line three: I can write y n m and more!"});
+
     const GridViewStateDTO viewStateDTO = m_gridService.retrieveGridViewState();
     m_view.updateGridViewState(viewStateDTO);
 }
