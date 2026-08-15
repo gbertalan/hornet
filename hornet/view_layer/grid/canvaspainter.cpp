f11cb4a3e78eec168a0b94f95ccdef0938b4c610
Gergely Bertalan <bertalanofficial@gmail.com>
2026-08-15 23:33:15 +0200

    Fix bug: line too thick when fully zoomed out, max(1,...) needs to be applied to the zoomed thickness, not the starting thickness

--- hornet/view_layer/grid/canvaspainter.cpp
+++ hornet/view_layer/grid/canvaspainter.cpp
@@ -229,7 +229,6 @@ void CanvasPainter::drawToolScriptPrimitives(QPainter &painter,
                                              FontRenderer &fontRenderer,
                                              float textScale)
 {
-    const double baseThickness = std::max(1.0, gridGap * 0.1);
     painter.save();
     painter.setBrush(Qt::NoBrush);
 
@@ -238,8 +237,8 @@ void CanvasPainter::drawToolScriptPrimitives(QPainter &painter,
         const double ly1 = geom.screenY + geom.headerH + toolLine.y1 * gridGap;
         const double lx2 = geom.screenX + toolLine.x2 * gridGap;
         const double ly2 = geom.screenY + geom.headerH + toolLine.y2 * gridGap;
-        painter.setPen(QPen(resolveToolColor(toolLine.colorToken, Theme::brightAmber()),
-                            baseThickness * toolLine.thicknessMultiplier));
+        const double thickness = std::max(1.0, gridGap * 0.1 * toolLine.thicknessMultiplier);
+        painter.setPen(QPen(resolveToolColor(toolLine.colorToken, Theme::brightAmber()), thickness));
         painter.drawLine(QPointF(lx1, ly1), QPointF(lx2, ly2));
     }
 
@@ -248,8 +247,8 @@ void CanvasPainter::drawToolScriptPrimitives(QPainter &painter,
         const double ry = geom.screenY + geom.headerH + toolRect.y * gridGap;
         const double rw = toolRect.width * gridGap;
         const double rh = toolRect.height * gridGap;
-        painter.setPen(QPen(resolveToolColor(toolRect.colorToken, Theme::brightAmber()),
-                            baseThickness * toolRect.thicknessMultiplier));
+        const double thickness = std::max(1.0, gridGap * 0.1 * toolRect.thicknessMultiplier);
+        painter.setPen(QPen(resolveToolColor(toolRect.colorToken, Theme::brightAmber()), thickness));
         painter.drawRect(QRectF(rx, ry, rw, rh));
     }
 
@@ -257,8 +256,9 @@ void CanvasPainter::drawToolScriptPrimitives(QPainter &painter,
         const double cx = geom.screenX + toolCircle.x * gridGap;
         const double cy = geom.screenY + geom.headerH + toolCircle.y * gridGap;
         const double r = toolCircle.radius * gridGap;
-        painter.setPen(QPen(resolveToolColor(toolCircle.colorToken, Theme::brightAmber()),
-                            baseThickness * toolCircle.thicknessMultiplier));
+        const double thickness = std::max(1.0, gridGap * 0.1 * toolCircle.thicknessMultiplier);
+        painter.setPen(
+            QPen(resolveToolColor(toolCircle.colorToken, Theme::brightAmber()), thickness));
         painter.drawEllipse(QPointF(cx, cy), r, r);
     }
 
